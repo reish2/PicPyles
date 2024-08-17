d76a704e13ba53a895f1ccb60e0b235beb357fa7
EthernalMesh <115874352+reish2@users.noreply.github.com>
2024-08-17 22:19:55 +0200

    adds buggy multi-object select and move

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from controllers.main_controller import PicPylesController
 from models.geometry import Triangle, ImageObject
 
@@ -7,6 +9,15 @@ def main():
     t1 = Triangle(color=(0.0, 1.0, 0.0), center=(-1,0,0))
     t2 = Triangle(color=(1.0, 0.0, 0.0), center=(1, 0, 0))
     i1 = ImageObject("assets/mascott.png", position=(0, 2, 0), size=(2.0, 2.0))
+
+    imgs = []
+    for k in range(10):
+        pos = np.random.rand(3)
+        pos[2] = 0
+        img = ImageObject("assets/mascott.png", position=pos, size=(2.0, 2.0))
+        imgs.append(img)
+        controller.add_object(img)
+
     controller.add_object(t1)
     controller.add_object(t2)
     controller.add_object(i1)
