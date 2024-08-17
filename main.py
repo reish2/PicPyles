6417f2eb2ade81e5c774fa1b381b7f3a392ee25b
EthernalMesh <115874352+reish2@users.noreply.github.com>
2024-08-17 17:03:28 +0200

    refactors Triangle to SceneObject baseclass

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from controllers.main_controller import PicPylesController
+from models.geometry import Triangle
 
 def main():
     controller = PicPylesController()
     # controller.add_triangle(color=(1.0, 0.0, 0.0), vertices=[(-1.0, -1.0, 0.0), (1.0, -1.0, 0.0), (0.0, 1.0, 0.0)])
-    controller.add_triangle(color=(0.0, 1.0, 0.0), center=(-1,0,0))
-    controller.add_triangle(color=(1.0, 0.0, 0.0), center=(1, 0, 0))
+    t1 = Triangle(color=(0.0, 1.0, 0.0), center=(-1,0,0))
+    t2 = Triangle(color=(1.0, 0.0, 0.0), center=(1, 0, 0))
+    controller.add_object(t1)
+    controller.add_object(t2)
     controller.run()
 
 if __name__ == "__main__":
--- models/geometry.py
+++ models/geometry.py
@@ -1,7 +1,7 @@
 from OpenGL.GL import *
 import numpy as np
 
-class Triangle:
+class SceneObject:
     def __init__(self, color, vertices):
         self.color = color
         self.vertices = vertices
@@ -13,16 +13,8 @@ class Triangle:
             glVertex3f(*vertex)
         glEnd()
 
-class Triangle2:
+class Triangle(SceneObject):
     def __init__(self, color, center):
         self.color = color
         self.center = np.array(center)
-        self.vertices = np.array(((-0.5, -0.5, 0.0), (0.5, -0.5, 0.0), (0.0, 0.5, 0.0)))
-
-    def render(self):
-        glBegin(GL_TRIANGLES)
-        glColor3f(*self.color)
-        vertices = self.vertices + self.center
-        for vertex in vertices:
-            glVertex3f(*vertex)
-        glEnd()
+        self.vertices = np.array(((-0.5, -0.5, 0.0), (0.5, -0.5, 0.0), (0.0, 0.5, 0.0))) + self.center
