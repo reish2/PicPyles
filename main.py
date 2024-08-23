e57b69fffddad44ed4041ca2fd94fbe05baca35b
glilley <g@lilley>
2024-08-23 20:50:58 +0200

    adds minimal appsettings feature
    that saves the last viewed folder on close of the app

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from controllers.controller import Controller
 
 
 def main():
-    # TODO: appsettings folder in user home
-    controller = Controller("~/Downloads/sources/PicPyles/data/test_pile_folder")
+    controller = Controller()
     controller.run()
 
 
